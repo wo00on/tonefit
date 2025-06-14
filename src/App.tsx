86d281770be6b84f7ed2bb37b3d5195947528527
woon <woony8959@gmail.com>
2025-06-14 19:09:51 +0900

    "start"

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+import HomePage from "./componants/HomePage.tsx";
+
+
+function App() {
+    return (
+        <div>
+            <HomePage />
+        </div>
+    )
+}
+
+export default App
